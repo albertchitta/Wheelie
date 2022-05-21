11f5bb59f73e26b84a5ffbb966db623193037cc0
albertchitta <albert.w.chittaphong@gmail.com>
2022-05-21 09:35:10 -0500

    Created models

---
+++ Wheelie/Models/Trail.cs
@@ -0,0 +1,12 @@
+﻿namespace Wheelie.Models
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public double Distance { get; set; }
+        public int Grade { get; set; }
+    }
+}
